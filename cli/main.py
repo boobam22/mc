434b5d104c6922ee9c903075259aa5c8ab25a03d
nia <mangrove0720@outlook.com>
2025-06-01 08:49:55 +0800

    add Paths

--- cli/main.py
+++ cli/main.py
@@ -1,5 +1,6 @@
 # pyright: reportUnusedImport=false
 
+import pathlib
 from parser import parser
 import typing as t
 
@@ -9,14 +10,21 @@ import commands.remove
 import commands.prune
 import commands.list
 import commands.start
+from types.path import Paths, VersionPaths
 
 if t.TYPE_CHECKING:
-    from types.args import BaseArgsDeprecated
+    from types.args import BaseArgs
 
 
 def main():
-    args = t.cast("BaseArgsDeprecated", parser.parse_args())
-    args.callback(args)
+    args = t.cast("BaseArgs", parser.parse_args())
+
+    if args.version is None:
+        ctx = Paths(args.root_path)
+    else:
+        ctx = VersionPaths(args.version, args.root_path)
+
+    args.callback(ctx)
 
 
 if __name__ == "__main__":
--- cli/types/args.py
+++ cli/types/args.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass
 from pathlib import Path
 import typing as t
 
+if t.TYPE_CHECKING:
+    from types.path import Paths
+
 
 @dataclass
 class BaseArgsDeprecated:
@@ -14,3 +17,11 @@ class BaseArgsDeprecated:
     VERSION_MANIFEST: Path
 
     callback: t.Callable[["BaseArgsDeprecated"], None]
+
+
+@dataclass
+class BaseArgs:
+    root_path: str | None
+    version: str | None
+
+    callback: t.Callable[["Paths"], None]
