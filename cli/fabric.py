354f10427f173ed011a2f4fd1d25ebdbb93e1ca0
nia <mangrove0720@outlook.com>
2025-06-01 10:00:46 +0800

    ref fabric

--- cli/fabric.py
+++ cli/fabric.py
@@ -4,10 +4,9 @@ import typing as t
 from client import client, download_all
 
 if t.TYPE_CHECKING:
-    from pathlib import Path
-
-    from types.json_schema import FabricMeta
     from client import URI
+    from types.path import VersionPaths
+    from types.json_schema import FabricMeta
 
 META_HOST = "https://meta.fabricmc.net"
 MAVEN_HOST = "https://maven.fabricmc.net"
@@ -20,29 +19,27 @@ def parse_maven(maven: str):
     return f"{path}/{name}/{version}/{name}-{version}.jar"
 
 
-async def install_fabric(local_dir: "Path", version: str):
-    metadata = local_dir / "fabric-metadata.json"
-
-    if metadata.exists():
+async def install_fabric(ctx: "VersionPaths"):
+    if ctx.fabric_metadata.exists():
         return
+
+    version = ctx.version_dir.name
 
     url = f"{META_HOST}/v2/versions/loader/{version}"
     res = await client.get(url)
     data: "FabricMeta" = res.json()[0]
 
-    lib_local_dir = local_dir / "libraries"
-
     url = f"{META_HOST}/v2/versions/loader/{version}/{data['loader']['version']}/profile/json"
-    items: list["URI"] = [(url, metadata, None)]
+    items: list["URI"] = [(url, ctx.fabric_metadata, None)]
 
     for key in ["loader", "intermediary"]:
         key = t.cast(t.Literal["loader", "intermediary"], key)
         path = parse_maven(data[key]["maven"])
-        items.append((f"{MAVEN_HOST}/{path}", lib_local_dir / path, None))
+        items.append((f"{MAVEN_HOST}/{path}", ctx.lib_dir / path, None))
 
     for item in data["launcherMeta"]["libraries"]["common"]:
         path = parse_maven(item["name"])
-        items.append((f"{MAVEN_HOST}/{path}", lib_local_dir / path, item["size"]))
+        items.append((f"{MAVEN_HOST}/{path}", ctx.lib_dir / path, item["size"]))
 
     await download_all(items)
 
@@ -51,6 +48,3 @@ async def install_fabric(local_dir: "Path", version: str):
         for item in jar.parent.parent.iterdir():
             if item.name != version:
                 shutil.rmtree(item)
-
-    main_class = local_dir / "MAINCLASS"
-    main_class.write_text(data["launcherMeta"]["mainClass"]["client"])
