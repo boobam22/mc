fd032b5e3009230b772c7cf20cbd424ed6d45dfe
nia <mangrove0720@outlook.com>
2025-07-11 03:20:47 +0800

    parse root-path and version

--- cli/main.py
+++ cli/main.py
@@ -2,11 +2,18 @@
 
 from parser import parser
 
+from context import context as ctx
 import commands.update
 
 
 def main():
     args = parser.parse_args()
+
+    if getattr(args, "root_path", None) is not None:
+        ctx.root = args.root_path
+    if getattr(args, "version", None) is not None:
+        ctx.version = args.version
+
     args.callback()
 
 
